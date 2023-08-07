85ca3a6d684f9f436bc44103625346114546d089
renata-aline <renatalinec@gmail.com>
2023-08-07 15:44:40 -0300

    captando eventos

---
+++ js/criarVideos.js
@@ -0,0 +1,12 @@
+const formulario = document.querySelector("[data-formulario]");
+
+function criarVideo(evento){
+    evento.preventDefault();
+
+    const imagem = document.querySelector("[data-imagem]").value;
+    const url = document.querySelector("[data-url]").value;
+    const titulo = document.querySelector("[data-titulo]").value;
+    const descricao = Math.floor(Math.randoom() * 10).toString();
+}
+
+formulario.addEventListener("submit", evento => criarVideo(evento));
